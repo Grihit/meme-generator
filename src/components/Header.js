c6cd76f5bc8a29f35a3838b26259863b0e59105f
Grihit <grihitbudhiraja12@gmail.com>
2022-04-21 18:54:10 +0530

    Added meme text

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
         <header className="header">
             <img src='images/troll-face.png' alt="Troll face" />
             <span className="header--title">Meme Generator</span>
-            <span className="header--course">React Course - Project 3</span>
+            <span className="header--course">Learning React</span>
         </header>
     )
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,23 +2,62 @@ import React from "react"
 import memesData from "../memesData"
 
 export default function Meme(){
-    const [memeUrl, setMemeUrl] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: ""
+    })
+    //const [allMemeImages, setAllMemeImages] = React.useState(memesData)
     function getMemeImage(){
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[randomNumber].url
-        return setMemeUrl(url)
+        return setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                randomImage: url
+            }
+        })
     }
-
+    function handleChange(event){
+        const {name,value} = event.target
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+    function handleSubmit(event){
+        event.preventDefault()
+    }
     return(
         <main className="meme">
             <div className="meme--form">
-                <div className="flex--container">
-                    <input type="text" placeholder="Enter Top Text"/>
-                    <input type="text" placeholder="Enter Bottom Text" />
+                <form onSubmit={handleSubmit}>
+                    <div className="flex--container">
+                        <input 
+                            type="text" 
+                            placeholder="Enter Top Text"
+                            value={meme.topText}
+                            name="topText"
+                            onChange={handleChange}
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Enter Bottom Text"
+                            value={meme.bottomText}
+                            name="bottomText"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <button onClick={getMemeImage} className="meme--button">Get a new meme image</button>
+                </form>
+                <div className="meme--image--div">
+                    {meme.randomImage && <img src={meme.randomImage} alt="Meme" className="meme--img" />}
+                    {meme.topText && meme.randomImage &&<h2 className="top meme--text" >{meme.topText}</h2>}
+                    {meme.bottomText && meme.randomImage &&<h2 className="bottom meme--text" >{meme.bottomText}</h2>}
                 </div>
-                <button onClick={getMemeImage} className="meme--button">Get a new meme image</button>
-                {memeUrl && <img src={memeUrl} alt="Meme Image" className="meme--img" />}
             </div>
         </main>
     )
